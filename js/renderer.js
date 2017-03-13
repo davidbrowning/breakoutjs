bf0449f177071f2784f90e9e953e29742a76a85e
dmbrowning21 <dave.browning21@aggiemail.usu.edu>
2017-03-13 09:51:42 -0600

    working on brick collision

--- js/renderer.js
+++ js/renderer.js
@@ -80,10 +80,8 @@ MyGame.graphics = (function() {
 			spec.rotation -= spec.rotateRate * (elapsedTime / 1000);
 		};
         that.checkPaddle = function(elapsedTime, x, w, new_center){
-            //new_center = spec.center.y + spec.moveRate * (elapsedTime/1000);
-            //console.log(Math.floor(spec.center.y)+'=='+canvas.height+' || '+Math.floor(spec.center.y)+ '==' +(canvas.height + 1))
             if(Math.floor(spec.center.y) == canvas.height-120 || Math.floor(spec.center.y) == canvas.height - 119){
-                console.log('hit')
+                //console.log('hit')
                 if(spec.center.x > x-(w/2) && spec.center.x < (x+(w/2))){
                     var alt_center = spec.center.y - spec.moveRate * (elapsedTime/1000);
                     new_center = alt_center
@@ -143,8 +141,34 @@ MyGame.graphics = (function() {
             }
 		};
 
+        that.checkRow = function(mx, row){
+            //console.log(mx)
+            //If I think about it, I might be able to derive an equation to 
+            // get which two(?) bricks it could be colliding with
+            // spec.center.x (~50 for the sake of example) / canvas.width 500 = .1
+            // spec.center.x (~250) / canvas.width (500) = 
 
-        that.validMove = function(elapsedTime, x, w){
+        }
+
+        that.checkCollisions = function(brickMatrix){
+            //if ball_y is greater than the value of the lowest brick, return
+            //else, determine ball_x and check against row w/in ball_y
+            if(typeof(brickMatrix)!='undefined'){
+                var row = 0
+                if(spec.center.y < 150 && spec.center.y > 130){console.log('blue');that.checkRow(brickMatrix[4])}    
+                if(spec.center.y < 130 && spec.center.y > 110){console.log('pink');that.checkRow(brickMatrix[3])}    
+                if(spec.center.y < 110 && spec.center.y > 90){console.log('yellow');that.checkRow(brickMatrix[2])}    
+                if(spec.center.y < 90 && spec.center.y > 70){console.log('green');that.checkRow(brickMatrix[1])}    
+                if(spec.center.y < 70 && spec.center.y > 50){console.log('red');that.checkRow(brickMatrix[0])}    
+                //if(spec.center.y < 50){alert('top')}
+                    
+                //console.log('x:',spec.center.x, 'y',spec.center.y);
+            }
+
+        }
+
+        that.validMove = function(elapsedTime, x, w, brickMatrix){
+            that.checkCollisions(brickMatrix)
             if(that.current_x == 1){
                 that.moveRight(elapsedTime)
             }
@@ -167,12 +191,9 @@ MyGame.graphics = (function() {
 
         }
         
-        that.move = function(elapsedTime, x, w){
-                if(x == undefined || w == undefined){
-                    alert('fucker')
-                }
-            that.validMove(elapsedTime, x, w)
-            //Was working here, pop the next two elements off the course array, then move accordingly.
+        that.move = function(elapsedTime, x, w, brickMatrix){
+            that.validMove(elapsedTime, x, w, brickMatrix)
+            
         }
 
         that.stickToPaddle = function(pos, elapsedTime){
@@ -289,10 +310,114 @@ MyGame.graphics = (function() {
 		return that;
 	}
 
+	function Brick(spec) {
+		var that = {},
+			ready = false,
+			image = new Image();
+		
+		//
+		// Load the image, set the ready flag once it is loaded so that
+		// rendering can begin.
+		image.onload = function() { 
+			ready = true;
+		};
+		image.src = spec.image;
+		
+        that.getPosition = function(){
+            return spec.center.x
+        }
+
+        that.getWidth = function(){
+            return spec.width
+        }
+		
+        that.explode = function(){
+
+        }
+
+		that.draw = function() {
+			if (ready) {
+				context.save();
+				
+				context.translate(spec.center.x, spec.center.y);
+				context.rotate(spec.rotation);
+				context.translate(-spec.center.x, -spec.center.y);
+				
+				context.drawImage(
+					image, 
+					spec.center.x - spec.width/2, 
+					spec.center.y - spec.height/2,
+					spec.width, spec.height);
+				
+				context.restore();
+			}
+		};
+		
+		return that;
+	}
+
+	function BrickMatrix(spec) {
+		var that = {},
+			ready = false,
+            mx = [];
+            
+    
+		
+        for(var i = 0; i < 5; i++){
+            var img
+            switch(i){
+                case 0:
+                    img = 'images/brickRed.png' 
+                    break;
+                case 1: 
+                    img = 'images/brickGreen.png' 
+                    break;
+                case 2: 
+                    img = 'images/brickOrange.png' 
+                    break;
+                case 3: 
+                    img = 'images/brickPink.png' 
+                    break;
+                default: 
+                    img = 'images/paddle.png' 
+            }
+            //console.log(img)
+            mx.push(new Array())
+            for(var j = 0; j < 10; j++){
+                mx[i].push(Brick({
+	        		image : img,
+	        		center : { x : (j+.5)*100, y : (i+1)*25 },
+	        		width : 95, height : 20,
+	        		moveRate : 500,			// pixels per second
+	        		rotateRate : 3.14159	// Radians per second
+	        	}))
+            }
+        }
+        ready = true;
+
+        that.getBricks = function(){
+            return mx
+        }
+
+		that.draw = function() {
+			if (ready) {
+                //TODO draw each brick still alive in the matrix
+                mx.forEach(function(row){
+                    row.forEach(function(cell){
+                        cell.draw()
+                    })
+                })
+			}
+		};
+        return that;
+    }
+
+
 	return {
 		clear : clear,
 		Texture : Texture,
         Ball : Ball,
+        BrickMatrix : BrickMatrix,
 	};
 }());
 
@@ -304,6 +429,7 @@ MyGame.graphics = (function() {
 MyGame.main = (function(graphics, input) {
 	'use strict';
 
+    var drawBricks = true;
 	var lastTimeStamp = performance.now(),
 		myKeyboard = input.Keyboard(),
 		myTexture = graphics.Texture( {
@@ -314,6 +440,12 @@ MyGame.main = (function(graphics, input) {
 			rotateRate : 3.14159	// Radians per second
 		});
 
+
+    var myBricks = graphics.BrickMatrix({
+            mx : [[]]
+        });
+
+
 	var	myBall = graphics.Ball( {
 			image : 'images/Ball.png',
 			center : { x : 100, y : 375 },
@@ -321,7 +453,9 @@ MyGame.main = (function(graphics, input) {
 			moveRate : 100,			// pixels per second
 			rotateRate : 3.14159	// Radians per second
 		});
-    myBall.newGame()
+
+
+    myBall.newGame(drawBricks)
 
 	//------------------------------------------------------------------
 	//
@@ -344,7 +478,7 @@ MyGame.main = (function(graphics, input) {
         }
         else{
             //console.log('move called')
-            myBall.validMove(elapsedTime, myTexture.getPosition(), myTexture.getWidth())
+            myBall.validMove(elapsedTime, myTexture.getPosition(), myTexture.getWidth(), myBricks.getBricks())
             myBall.move(elapsedTime, myTexture.getPosition(), myTexture.getWidth())    
         }
 	}
@@ -354,10 +488,13 @@ MyGame.main = (function(graphics, input) {
 	// Render the state of the "model", which is just our texture in this case.
 	//
 	//------------------------------------------------------------------
-	function render() {
-		graphics.clear();
+	function render(elapsedTime) {
+        if(Math.floor(elapsedTime) > 3){
+		    graphics.clear();
+        }
 		myTexture.draw();
         myBall.draw();
+        myBricks.draw();
 	}
 
 	//------------------------------------------------------------------
@@ -372,7 +509,7 @@ MyGame.main = (function(graphics, input) {
 
 		processInput(elapsedTime);
 		update(elapsedTime);
-		render();
+		render(elapsedTime);
 
 		requestAnimationFrame(gameLoop);
 	};
@@ -393,3 +530,4 @@ MyGame.main = (function(graphics, input) {
 	requestAnimationFrame(gameLoop);
  
 }(MyGame.graphics, MyGame.input));
+
